604f7e56cfe64df22f98df05f4ea8ccfb5660000
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-08-15 22:53:32 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
 // Code SimpleComponent Here
+import React from 'react';
+
+export default class SimpleComponent extends React.Component {
+
+	constructor(props) {
+		super()
+		this.state = {
+			mood: "happy"
+		}
+	}
+
+	handleClick = () => {
+		this.setState({
+			mood: "sad"
+		})
+	}
+
+	render() {
+		return (
+			<div className="mood" 
+			onClick={this.handleClick}>
+			{this.state.mood}
+			</div>
+		)
+	}
+
+}
